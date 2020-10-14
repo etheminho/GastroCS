abf08ca27541e41a9368d94832a11238dd215b03
Mustafa Y <mustafa.yologlu@web.de>
2020-10-14 13:00:09 +0200

    Added Toast

--- app/src/main/java/com/example/gastrocs/MainActivity.java
+++ app/src/main/java/com/example/gastrocs/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gastrocs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Awesome", Toast.LENGTH_SHORT).show();
     }
 }
